19f133a72425cc745d3d0984e93caf43e9f6862c
maykbsc <maykastudillo08@gmail.com>
2018-10-30 20:42:02 -0500

    1 error+ doc instruciones

---
+++ solid/src/_0srp/bien/Curacion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package _0srp.bien;
+
+/**
+ *
+ * @author Maykel
+ */
+public class Curacion {
+   
+    public void curar(Pokemon poke){
+        poke.setPorcentajeVida(100);
+    }
+    
+}
